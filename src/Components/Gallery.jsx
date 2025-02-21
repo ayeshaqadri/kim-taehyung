0e31a6ec3ae3f732e1336efd0c83547467befeed
ayeshaqadri <qadria392@gmail.com>
2025-02-21 20:28:03 +0500

    'inprocess'

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -3,14 +3,26 @@ import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import './Gallery.css'
 import { useState } from "react";
-import Kim1 from "../Images/KIM-1.jpg";  // Add your image paths
+import Kim1 from "../Images/KIM-1.jpg";  
 import Kim2 from "../Images/KIM-2.jpg";
 import Kim3 from "../Images/KIM-3.jpg";
 import Kim4 from "../Images/KIM-4.jpeg";
 import Kim5 from "../Images/KIM-5.jpeg";
 import Kim6 from "../Images/KIM-6.jpeg";
 import Kim7 from "../Images/KIM-7.jpeg";
-// import Kim10 from "../Images/KIM-10.jpeg";
+import Kim8 from "../Images/KIM-8.jpg";
+import Kim9 from "../Images/KIM-9.jpg";
+import Kim10 from "../Images/KIM-10.jpg";
+import Kim11 from "../Images/KIM-11.jpg";
+import Kim12 from "../Images/KIM-12.jpg";
+import Kim13 from "../Images/KIM-13.jpg";
+import Kim14 from "../Images/KIM-14.jpg";
+import Kim15 from "../Images/KIM-15.jpg";
+import Kim16 from "../Images/KIM-16.jpg";
+import Kim17 from "../Images/KIM-17.jpg";
+import Kim18 from "../Images/KIM-18.jpg";
+import Kim19 from "../Images/KIM-19.jpg";
+import Kim20 from "../Images/KIM-20.jpg";
 
 
 const images = [
@@ -21,7 +33,19 @@ const images = [
   { src: Kim5 },
   { src: Kim6 },
   { src: Kim7 },
-  // { src: Kim8 },
+  { src: Kim8 },
+  { src: Kim9 },
+  { src: Kim10 },
+  { src: Kim11 },
+  { src: Kim12},
+  { src: Kim13 },
+  { src: Kim14 },
+  { src: Kim15},
+  { src: Kim16 },
+  { src: Kim17 },
+  { src: Kim18 },
+  { src: Kim19 },
+  { src: Kim20 },
 
 ];
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,25 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css"; 
-import "bootstrap/dist/js/bootstrap.bundle.min.js"; // 🔥 This is required for navbar toggle
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Required for navbar toggle
+import logo from '../Images/kim-logo-2-removebg-preview.png'; // Ensure the correct file extension
 
-function navbar() {
+function NavbarComponent() {
   return (
-    <Navbar expand="lg" className="navbar-custom fadeIn"> 
+    <Navbar expand="lg" className="navbar-custom fadeIn">
       <Container>
-        <Navbar.Brand href="#" style={{ color: "purple", fontWeight: "bold" }}>
-          V's World
+        <Navbar.Brand href="#">
+          <img 
+            src={logo} 
+            alt="Logo" 
+            style={{ height: "200px", width: "200px" }} // Adjust size as needed
+          />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" style={{ color: "purple", background: "white" }}/>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" style={{ color: "purple", background: "white" }} />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#">Gallery</Nav.Link>
             <Nav.Link href="#">About</Nav.Link>
-            <Nav.Link href="#">Contact</Nav.Link>
+            <Nav.Link href="#">Kim's Gallery</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -23,5 +27,4 @@ function navbar() {
   );
 }
 
-export default navbar;
-
+export default NavbarComponent;
