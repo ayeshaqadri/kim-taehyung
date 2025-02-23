e809cfdd7029821ba8b178905c63c04d3f60a896
ayeshaqadri <qadria392@gmail.com>
2025-02-23 14:19:40 +0500

    'ok'

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 import "./HeroSection.css"; // Import CSS for styling
-import MainPic from "../Images/kim-taehyung-main.webp"; // Ensure correct path
 
 function HeroSection() {
+  const navigate = useNavigate(); // Initialize navigate function
+
+  const handleNavigation = () => {
+    navigate("/gallery"); // Navigate to the Gallery page
+  };
+
   return (
     <div className="hero">
       <div className="hero-content slideInLeft">
         <h1>Welcome to<br /> V's World</h1>
         <p>Explore the artistry and talent of Kim Taehyung</p>
-        <button className="btn-1 btn-purple">
-          <span>Discover More</span>
+        <button className="btn-1 btn-purple" onClick={handleNavigation}>
+          <span>A Glimpse of V</span>
         </button>
       </div>
     </div>
